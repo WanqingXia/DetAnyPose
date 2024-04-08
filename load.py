9bd15c4c9512e09475e87ab4a7cfd70b22b8f3f6
iai-hrc <45677464+WanqingXia@users.noreply.github.com>
2024-04-08 14:22:28 +1200

    Delete load.py

--- load.py
+++
@@ -1,6 +0,0 @@
-import torch
-from torchsummary import summary
-
-dinov2_vitg14_reg = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14_reg').cuda()
-summary(dinov2_vitg14_reg, input_size=(3, 224, 224))  # Adjust the input size (3, 224, 224) as per your model
-
