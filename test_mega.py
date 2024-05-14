add409f1ee2b37549bd91119cedb467f03fda2c6
iai-hrc <45677464+WanqingXia@users.noreply.github.com>
2024-05-14 16:05:39 +1200

    fixed hang on multiprocessing in megapose

--- test_mega.py
+++ test_mega.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-# from mmdet_sam import mmdet_sam
-# from fbdinov2 import fbdinov2
+from mmdet_sam import mmdet_sam
+from fbdinov2 import fbdinov2
 from megapose import nvmegapose
 
 device = 'cuda:0'
-# MMDet_SAM = mmdet_sam.MMDet_SAM(device)
-# DINOv2 = fbdinov2.DINOv2("./viewpoints_42", device)
-
 Megapose = nvmegapose.Megapose(device)
+MMDet_SAM = mmdet_sam.MMDet_SAM(device)
+DINOv2 = fbdinov2.DINOv2("./viewpoints_42", device)
+
+
 Megapose.run_inference(Path('./data/drill'))
